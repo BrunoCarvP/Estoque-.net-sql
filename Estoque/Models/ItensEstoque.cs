dd93fe756d9419b60cf530beb51727b5ec642c63
Bruno de Carvalho <bruno09carvalho@outlook.com>
2023-11-13 23:44:40 -0300

    v2

--- Estoque/Models/ItensEstoque.cs
+++ Estoque/Models/ItensEstoque.cs
@@ -2,19 +2,14 @@
 
 namespace Estoque.Models
 {
-    public enum SetorEnum
-    {
-        Cozinha,
-        Limpeza,
-        Fabrica,
-        Administração
-    }
+    
     public class ItensEstoque
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Setor { get; set; } // Alteração da propriedade de Descrição para Setor
         public int Quantidade { get; set; }
+
 
         // Propriedade que retorna true se a quantidade for menor ou igual a 10
         public bool PrecisaReabastecer => Quantidade <= 10;
